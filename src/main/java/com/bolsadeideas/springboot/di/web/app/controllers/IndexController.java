435e06b8d9bedcd3171dd17111a439017e674f1e
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-03-25 11:41:30 -0600

    Agredando la lista y el perfil

--- src/main/java/com/bolsadeideas/springboot/di/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/web/app/controllers/IndexController.java
@@ -1,9 +1,14 @@
 package com.bolsadeideas.springboot.di.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.di.web.app.models.Usuario;
 
 //Un controlador debe de tener la notacion controller para que se pueda cargar por la clase principal
 @Controller
@@ -21,5 +26,26 @@ public class IndexController {
 		//El archivo index.html debe de existir en la ruta src/main/resources/templates/index.html
 		//y debe de ser una pagina html
 		return "index";
+	}
+	
+	@RequestMapping("perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Pedro");
+		usuario.setApellido("Flores");
+		model.addAttribute("usuario",usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
+	}
+	
+	@RequestMapping("listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(new Usuario("Pedro","Flores","pefloresc@gmail.com"));
+		usuarios.add(new Usuario("Sofia","Castillo","socastillot@gmail.com"));
+		usuarios.add(new Usuario("Leonor","Flores","leflorest@gmail.com"));
+		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";
 	}
 }

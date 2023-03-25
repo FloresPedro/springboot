713c3b5d117973cb6dab33a936c225bd6a1a5b94
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-03-25 13:15:41 -0600

    Complementado mas metodos para paso de parametros por la url

--- src/main/java/com/bolsadeideas/springboot/di/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/di/web/app/controllers/EjemploParamsController.java
@@ -9,10 +9,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
+	/**
+	 * 
+	 * @param texto que viene desde la url
+	 * @param model componente de spring para juntar informacion 
+	 * @return una pagina html donde se muestra los parametros que viajaron por la url
+	 * para este ejemplo se espera que llegue una variable con el nombre de resultado
+	 * se pone el required en false para que en caso de que no vaya dicha informacion 
+	 * no truene al acceder al path
+	 * defaultValue para poner un valor si no llega nada por la url
+	 */
 
 	@GetMapping("/string")
-	public String param(@RequestParam(name = "texto") String texto, Model model) {
-		
+	public String param(@RequestParam(name = "texto", required = false, defaultValue = "no viajo valor por la url") String texto, Model model) {
+		model.addAttribute("resultado", "El resultado es: ".concat(texto));
 		return "params/ver";
 	}
+	
+	/**
+	 * metodo que redirecciona con informacion desde el html
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/")
+	public String index(Model model) {
+		
+		return "params/index";
+	}
+	
+	//Metodo que recibe mas de un parametro y de diferente tipo en la url
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String texto,@RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado", "El resultado es: ".concat(texto).concat(" y el numero es ").concat(numero.toString()) );
+		return "params/ver";
+	}
+	
 }

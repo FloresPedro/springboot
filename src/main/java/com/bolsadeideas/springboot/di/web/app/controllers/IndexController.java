5cd554d8769ede6022d9c1621f7f4077641f242b
Pedro Flores Cortes <florescortespedro94@gmail.com>
2023-03-25 12:42:25 -0600

    añadiendo un nuevo controler y pasando informacion

--- src/main/java/com/bolsadeideas/springboot/di/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/web/app/controllers/IndexController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.di.web.app.models.Usuario;
@@ -40,12 +41,21 @@ public class IndexController {
 	
 	@RequestMapping("listar")
 	public String listar(Model model) {
+		
+		model.addAttribute("titulo", "Listado de usuarios");
+		//al estar en un ModelAttribute la lista esta disponible para todos los
+		//metodos del controlador
+		//model.addAttribute("usuarios", usuarios);
+		return "listar";
+	}
+	
+	//otra forma de pasar datos a la vista
+	@ModelAttribute("usuarios")
+	public List<Usuario> listarUsuarios() {
 		List<Usuario> usuarios = new ArrayList<>();
 		usuarios.add(new Usuario("Pedro","Flores","pefloresc@gmail.com"));
 		usuarios.add(new Usuario("Sofia","Castillo","socastillot@gmail.com"));
 		usuarios.add(new Usuario("Leonor","Flores","leflorest@gmail.com"));
-		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
-		return "listar";
+		return usuarios;
 	}
 }
